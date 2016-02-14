6be395f69209b1054ac6c2a5c05d70d128c0b96e
Tobias Edwards <tobias.w.edwards@gmail.com>
2016-02-14 20:27:18 +1100

    Added main gcj function

---
+++ index.js
@@ -0,0 +1,27 @@
+var readline = require('readline');
+var GcjInput = require('lib/gcj-input.js');
+
+function loadLines(callback) {
+  var rl = readline.createInterface({ input: process.stdin });
+
+  lines = [];
+  rl.on('line', function (line) {
+    lines.push(line);
+  });
+
+  rl.on('close', function () {
+    callback(lines);
+  });
+}
+
+exports = function (solution) {
+  loadLines(function (lines) {
+    var input = new GcjInput(lines);
+    var numCases = input.getInt();
+
+    for (var i = 0; i < numCases; i++) {
+      var output = 'Case #' + (i + 1) + ': ' + solution(input);
+      console.log(output);
+    }
+  });
+};
