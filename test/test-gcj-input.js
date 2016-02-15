ae077070c25d73dad49c4c4e393da880d2770dfb
Tobias Edwards <tobias.w.edwards@gmail.com>
2016-02-15 20:54:38 +1100

    Add test cases for GcjInput

---
+++ test/test-gcj-input.js
@@ -0,0 +1,31 @@
+var assert = require('assert');
+var GcjInput = require('../lib/gcj-input');
+
+describe('GcjInput', function () {
+  describe('#getInt()', function () {
+    it('should return the correct integer', function () {
+      var input = new GcjInput(['42']);
+      assert.equal(42, input.getInt());
+    });
+
+    it('should correctly handle negative integers', function () {
+      var input = new GcjInput(['-12']);
+      assert.equal(-12, input.getInt());
+    });
+  });
+
+  describe('#getIntArray()', function () {
+    it('should return the correct array', function () {
+      var input = new GcjInput(['1 12 4 8 -1 22']);
+      assert.deepEqual([1, 12, 4, 8, -1, 22], input.getIntArray());
+    });
+  });
+
+  describe('#consumeLine()', function () {
+    it('should correctly discard the current line', function () {
+      var input = new GcjInput(['1', '4']);
+      input.consumeLine();
+      assert.equal(4, input.getInt());
+    });
+  });
+});
